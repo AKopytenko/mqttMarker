09ef62957e333bf1ca807983ea18493bdc0c4138
AKopytenko <kopytenko.a@gmail.com>
2020-01-30 16:42:42 +0300

    trash cleaning

--- src/components/input/formInput.vue
+++ src/components/input/formInput.vue
@@ -25,34 +25,14 @@ export default {
     methods: {
         inputCoords: function() {
             let inputVal = this.val;
-
-            if(inputVal.length) {
-                if(this.inputId !== 'pointName') {
-                    if( /^-?\d{1,3}\.\d{0,6}$/.test(inputVal) && inputVal <= 180 && inputVal >= -180 ) {
-                        console.log('OK: Coords'); 
-                        return true;
-                    } else {
-                        console.log('FAIL: Coords'); 
-                        return false;
-                    }
-                } else {
-                    if( /^[A-Za-zА-Яа-я\d\s\.]{3,75}$/.test(inputVal) ) {
-                        console.log('OK: String'); 
-                        return true;
-                    } else {
-                        console.log('FAIL: String'); 
-                        return false;
-                    }
-                }
-
-                if( inputVal >= -180 ) {
-                    console.log('OK 3'); 
-                } else {
-                    console.log('FAIL'); 
-                    return false;
-                }
-
-                
+            if(this.inputId !== 'pointName') {
+                if( /^-?\d{1,3}\.\d{0,6}$/.test(inputVal) && inputVal <= 180 && inputVal >= -180 )
+                    return true;
+                return false;
+            } else {
+                if( /^[A-Za-zА-Яа-я\d\s\.]{3,75}$/.test(inputVal) )
+                    return true;
+                return false;
             }
         }
     },
